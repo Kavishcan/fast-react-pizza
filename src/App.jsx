4fb7e0d3989773c5a5dd68abec2b7691d0f6028e
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-07 13:40:49 +0530

    Writing Data With React Router Actions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -28,6 +30,7 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
