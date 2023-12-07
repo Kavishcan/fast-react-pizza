3ac88e3522da9189f18ef9f016b46e27c6748b70
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-07 12:44:52 +0530

    Fetching Orders

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-
     children: [
       {
         path: "/",
@@ -33,6 +32,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
