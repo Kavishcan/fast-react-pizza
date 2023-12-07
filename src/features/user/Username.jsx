607250e0de015eb919bf6fe063ffa64f4fb1eef1
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-08 01:07:43 +0530

    Styling Text

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">Jimmy</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>Jimmy</p>
+      <Username />
     </header>
   );
 }
