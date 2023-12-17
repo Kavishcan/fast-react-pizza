7ed87fc5b182ec16e4a831670ea7789103cea2b8
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-17 22:46:00 +0530

    Update package-lock.json and package.json versions, and fix formatting in CartOverview and CreateUser components

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 uppercase text-stone-200 px-4 py-4 text-sm md:text-base sm:px-6">
-      <p className="text-semibold text-stone-300 space-x-4 sm:space-x-6 ">
+    <div className="bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="text-semibold space-x-4 text-stone-300 sm:space-x-6 ">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
@@ -13,4 +13,3 @@ function CartOverview() {
 }
 
 export default CartOverview;
- 
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -9,14 +9,16 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className='mb-4 text-sm text-stone-600 md:text-base'>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className='w-72'
+        className="w-72"
       />
 
       {username !== '' && (
