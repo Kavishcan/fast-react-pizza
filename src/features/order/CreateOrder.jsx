d271b649c47fb6836af9672ebbb30b21390fe5f3
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-18 01:02:24 +0530

    Style Form Elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -21,7 +21,7 @@ const fakeCart = [
     name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
-    totalPrice: 13,
+    totalPrice: 13, 
   },
   {
     pizzaId: 11,
@@ -61,12 +61,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 w-full md:px-6  md:py-3"  type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
+          className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
