868fbb146e45145737f7315f422dbe8e6839c24b
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-18 00:26:54 +0530

    Use CSS Grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,15 +8,19 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      
+      <div className="overflow-scroll">
+        <main className="max-w-3xl mx-auto">
+          <Outlet /> 
+        </main>
+      </div>
+
       <CartOverview />
     </div>
-  );
+  ); 
 }
 
 export default AppLayout;
